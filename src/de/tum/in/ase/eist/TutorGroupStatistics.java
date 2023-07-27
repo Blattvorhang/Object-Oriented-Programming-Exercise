ee998d35e2bfc04b449e73f75f7e5c1c59835ea4
Blattvorhang <blattvorhang@163.com>
2023-07-27 19:15:32 +0200

    Object-Oriented Programming

--- src/de/tum/in/ase/eist/TutorGroupStatistics.java
+++ src/de/tum/in/ase/eist/TutorGroupStatistics.java
@@ -14,15 +14,15 @@ public final class TutorGroupStatistics {
 
 	public static double averageDuration(Stream<TutorGroupMeeting> meetingStream) {
 		return meetingStream.mapToLong(meeting -> meeting.getTutorGroup().getTimeSlot().getDuration().toSeconds())
-							.average()
-							.orElse(0);
+		                    .average()
+		                    .orElse(0);
 	}
 
 	public static Set<Skill> repeatedSkills(Stream<TutorGroupMeeting> meetings) {
 		List<Skill> skillsList = meetings.map(TutorGroupMeeting::getSkillToPractise)
-										 .toList();
+		                                 .toList();
 		return skillsList.stream()
-						 .filter(skill -> Collections.frequency(skillsList, skill) > 1)
-					 	 .collect(Collectors.toSet());
+		                 .filter(skill -> Collections.frequency(skillsList, skill) > 1)
+		                 .collect(Collectors.toSet());
 	}
 }
